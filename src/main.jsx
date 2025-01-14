fa8c7504ed07062778ecb41ca2479c18c0370a7d
prettycodedirector <prettycodedirector@gmail.com>
2025-01-14 08:31:55 +0530

    clean dist

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { render } from 'preact'
 import './index.css'
-import { App } from './app.jsx'
+import { App } from './app'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
